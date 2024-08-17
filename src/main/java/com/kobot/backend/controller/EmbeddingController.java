ec4a1fbf3944d89c33575aad873b11de6d65bc3e
vamlin <lsc991123@gmail.com>
2024-08-17 22:47:52 +0900

    refactor: EmbeddingController URL mapping 수정

--- src/main/java/com/kobot/backend/controller/EmbeddingController.java
+++ src/main/java/com/kobot/backend/controller/EmbeddingController.java
@@ -6,22 +6,24 @@ import javax.annotation.Nullable;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
+@RequestMapping("/embedding")
 @RequiredArgsConstructor
 public class EmbeddingController {
 
   private final EmbeddingService embeddingService;
 
-  @PostMapping("/embedding")
+  @PostMapping("/text")
   public void EmbeddingText(@RequestBody String text) {
     embeddingService.saveEmbedding(text);
   }
 
-  @PostMapping("/embedding/pdf")
+  @PostMapping("/pdf")
   public void EmbeddingPdf(@RequestPart("file") @Nullable MultipartFile file){
     embeddingService.savePdfEmbedding(Objects.requireNonNull(file));
   }
