50e9c79a02f84c252d4fe426f20c6943ba4da1fd
yuna1212 <yunachong26@gmail.com>
2024-08-19 23:25:21 +0900

    test: 로그 추가 및 lombok 설정 #3
    - CacheDto에 toString 구현

--- src/main/java/com/kobot/backend/CacheDto.java
+++ src/main/java/com/kobot/backend/CacheDto.java
@@ -3,10 +3,12 @@ package com.kobot.backend;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
 @AllArgsConstructor
 @Builder
+@ToString
 public class CacheDto {
 
     private String id;
--- src/test/java/com/kobot/backend/service/EsCacheServiceTest.java
+++ src/test/java/com/kobot/backend/service/EsCacheServiceTest.java
@@ -2,6 +2,7 @@ package com.kobot.backend.service;
 
 import com.kobot.backend.CacheDto;
 import com.kobot.backend.KobotBackendApplication;
+import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
 @ContextConfiguration(classes = KobotBackendApplication.class)
+@Slf4j
 public class EsCacheServiceTest {
 
     @Autowired
@@ -25,6 +27,7 @@ public class EsCacheServiceTest {
     void testGetCached() {
         CacheDto cached = cacheService.getCached("한국의 수도는?");
         Assertions.assertThat(cached).isNotNull();
+        log.info("{}", cached);
 
     }
 }
