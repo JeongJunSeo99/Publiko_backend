79bb5493c472fb7ff564f2175fb617e78ad60415
SeokCheol <lsc991123@gmail.com>
2024-08-27 14:05:27 +0900

    feat : controller 추가 및 controller aop 추가 #13

--- src/main/java/com/kobot/backend/controller/PromptController.java
+++ src/main/java/com/kobot/backend/controller/PromptController.java
@@ -1,6 +1,9 @@
 package com.kobot.backend.controller;
 
+import com.kobot.backend.CacheDto;
 import com.kobot.backend.service.PromptService;
+import com.kobot.backend.service.RedisCacheService;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,10 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class PromptController {
 
     private final PromptService promptService;
+    private final RedisCacheService redisCacheService;
 
     @PostMapping("/chat")
     public String getQueryResult(@RequestBody String query) {
         log.info(query);
         return promptService.getChatResponse(query);
+    }
+
+    @PostMapping("/chat-cache")
+    public String getCacheQueryResult(@RequestBody String query) {
+        CacheDto cacheDto = Optional.ofNullable(redisCacheService.getCached(query))
+            .orElseGet(() -> redisCacheService.cache(query, promptService.getChatResponse(query)));
+
+        return cacheDto.getAnswer();
     }
 }
